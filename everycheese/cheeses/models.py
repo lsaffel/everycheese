994db98e7a96da2e20815d18db9d7fdc2d41715d
Lisa <llvvss@icloud.com>
2021-07-03 16:53:49 -0600

    Update models.py

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -4,12 +4,6 @@ from model_utils.models import TimeStampedModel
 
 
 class Cheese(TimeStampedModel):
-    name = models.CharField("Name of Cheese", max_length=255)
-
-    slug = AutoSlugField("Cheese Address",
-                         unique=True, always_update=False, populate_from="name")
-
-    description = models.TextField("Description", blank=True)
 
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
@@ -19,5 +13,12 @@ class Cheese(TimeStampedModel):
         HARD = "hard", "Hard"
 
     # Other Fields Here...
+    name = models.CharField("Name of Cheese", max_length=255)
+
+    slug = AutoSlugField("Cheese Address",
+                         unique=True, always_update=False, populate_from="name")
+
+    description = models.TextField("Description", blank=True)
+
     firmness = models.CharField("Firmness", max_length=20,
                                 choices=Firmness.choices, default=Firmness.UNSPECIFIED)
