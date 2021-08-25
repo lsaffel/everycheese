b681605a4698604cf280ec2efe54e912e6d22c5f
Lisa <llvvss@icloud.com>
2021-08-24 19:53:08 -0600

    Add cheeses app

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -20,3 +20,6 @@ class Cheese(TimeStampedModel):
 
     firmness = models.CharField("Firmness", max_length=20,
                                 choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+
+    def __str__(self):
+        return self.name
