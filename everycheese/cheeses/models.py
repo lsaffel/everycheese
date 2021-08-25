012612905891ebc3f4618b63179d610656001b4a
Lisa <llvvss@icloud.com>
2021-08-24 19:15:41 -0600

    through adding firmness field - works

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -4,6 +4,10 @@ from model_utils.models import TimeStampedModel
 
 
 class Cheese(TimeStampedModel):
+    name = models.CharField("Name of Cheese", max_length=255)
+    slug = AutoSlugField("Cheese Address",
+                         unique=True, always_update=False, populate_from="name")
+    description = models.TextField("Description", blank=True)
 
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
@@ -13,12 +17,6 @@ class Cheese(TimeStampedModel):
         HARD = "hard", "Hard"
 
     # Other Fields Here...
-    name = models.CharField("Name of Cheese", max_length=255)
-
-    slug = AutoSlugField("Cheese Address",
-                         unique=True, always_update=False, populate_from="name")
-
-    description = models.TextField("Description", blank=True)
 
     firmness = models.CharField("Firmness", max_length=20,
                                 choices=Firmness.choices, default=Firmness.UNSPECIFIED)
